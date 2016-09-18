20fcc5c928cf27286d9d31c3e90404a192a10a20
juliusgau <juliusgau99@gmail.com>
2016-09-18 23:43:22 +0200

    Added C++ Version
    It is waaaaay slower

---
+++ Collatz.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+	int i;
+	unsigned int start = clock();
+	for (int k = 0; k < 10; k++)
+	{
+		for (int j = 1; j < 100000; j++)
+		{
+			i = j;
+			while (i != 1)
+			{
+				if ((i & 1) != 0)
+				{
+					i = ((3 * i) + 1) >> 1;
+				}
+				else if ((i & 1) == 0)
+				{
+					i = i >> 1;
+				}
+				else
+				{
+					i = 1;
+				}
+			}
+		}
+	}
+	unsigned int end = clock() - start;
+	std::cout << "Time taken in millisecs: " << end;
+	std::cin.ignore();
+	return 0;
+}
